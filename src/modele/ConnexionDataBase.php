0e80344479a92747043f55c52df85856694e6de2
Francis <kientegafrancis@gmail.com>
2022-08-01 18:30:55 +0200

    test de lancement

---
+++ src/modele/ConnexionDataBase.php
@@ -0,0 +1,24 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_DATABASE', 'immobilier');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_PORT', 3304);
+
+class ConnexionDataBase
+{
+	private static $db;
+
+	public static function getDBO()
+	{
+		try {
+			self::$db = new PDO("mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+			self::$db->exec("SET CHARACTER SET utf8");
+			self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e) {
+			echo 'Connexion échouée : ' . $e->getMessage();
+		}
+		return self::$db;
+	}
+}
+?>
